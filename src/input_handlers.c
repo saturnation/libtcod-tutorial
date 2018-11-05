e70458658ebb59477add1fe3f3cf588e420ee1ae
saturnation <44737354+saturnation@users.noreply.github.com>
2018-11-05 22:08:00 +1000

    adding diagonal player movement

--- src/input_handlers.c
+++ src/input_handlers.c
@@ -11,25 +11,45 @@
 struct action* handle_keys(TCOD_key_t *k) {
     struct action *a = malloc(sizeof(struct action));
 
-    if (k->vk == TCODK_UP) {
+    if (k->vk == TCODK_UP || k->c == 'k') {
         a->type = MOVE;
         a->data.move.dx = 0;
         a->data.move.dy = -1;
         return a;
-    } else if (k->vk == TCODK_DOWN) {
+    } else if (k->vk == TCODK_DOWN || k->c == 'j') {
         a->type = MOVE;
         a->data.move.dx = 0;
         a->data.move.dy = 1;
         return a;
-    } else if (k->vk == TCODK_LEFT) {
+    } else if (k->vk == TCODK_LEFT || k->c == 'h') {
         a->type = MOVE;
         a->data.move.dx = -1;
         a->data.move.dy = 0;
         return a;
-    } else if (k->vk == TCODK_RIGHT) {
+    } else if (k->vk == TCODK_RIGHT || k->c == 'l') {
         a->type = MOVE;
         a->data.move.dx = 1;
         a->data.move.dy = 0;
+        return a;
+    } else if (k->c == 'y') {
+        a->type = MOVE;
+        a->data.move.dx = -1;
+        a->data.move.dy = -1;
+        return a;
+    } else if (k->c == 'u') {
+        a->type = MOVE;
+        a->data.move.dx = 1;
+        a->data.move.dy = -1;
+        return a;
+    } else if (k->c == 'b') {
+        a->type = MOVE;
+        a->data.move.dx = -1;
+        a->data.move.dy = 1;
+        return a;
+    } else if (k->c == 'n') {
+        a->type = MOVE;
+        a->data.move.dx = 1;
+        a->data.move.dy = 1;
         return a;
     } else if (k->vk == TCODK_ENTER && k->lalt) {
         a->type = FULLSCREEN;
